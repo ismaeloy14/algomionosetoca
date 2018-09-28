d2508186adc1988e33968ad911674350d423886c
Ismael <Ismael@LAPTOP-I75TTFFK>
2018-09-28 16:04:06 +0200

    commit inicial

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class BaseClass {
+               function __construct() {
+                   echo "En el constructor BaseClass";
+                   
+                   
+                
+   }
+}
+            
+
+        ?>
+    </body>
+</html>
